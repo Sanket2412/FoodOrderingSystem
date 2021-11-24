2039d0817e670940719ddaf59ca1faa9f8555ccd
Sanket Khardekar <khardekarsanket@gmail.com>
2021-11-24 23:37:19 +0530

    Food Items
    Added View for Food Items for manager and updated manager login page

---
+++ view_food_items.php
@@ -0,0 +1,165 @@
+<?php
+include('session_m.php');
+
+if(!isset($login_session)){
+header('Location: managerlogin.php'); // Redirecting To Home Page
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title> Manager Login | FRIENDS CORNER CAFE</title>
+  </head>
+
+  <link rel="stylesheet" type = "text/css" href ="css/view_food_items.css">
+<link rel="stylesheet" type = "text/css" href ="css/back.css">
+  <link rel="stylesheet" type = "text/css" href ="css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <body>
+
+
+    <nav class="navbar navbar-inverse navbar-fixed-top navigation-clean-search" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">FRIENDS CORNER CAFE</a>
+        </div>
+
+        <div class="collapse navbar-collapse " id="myNavbar">
+          <ul class="nav navbar-nav">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="aboutus.php">About</a></li>
+          </ul>
+
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="profile2.php"><span class="glyphicon glyphicon-user"></span> Welcome <?php echo $login_session; ?> </a></li>
+            <li class="active"> <a href="view_order_details.php">MANAGER CONTROL PANEL</a></li>
+            <li><a href="logout_m.php"><span class="glyphicon glyphicon-log-out"></span> Log Out </a></li>
+          </ul>
+        </div>
+
+      </div>
+    </nav>
+
+
+
+
+<div class="container">
+    <div class="jumbotron">
+     <h1>Hello Manager! </h1>
+     <p>Manage all your Cafe from here</p>
+
+    </div>
+    </div>
+
+<div class="container">
+    <div class="container">
+    	<div class="col">
+    		
+    	</div>
+    </div>
+
+    
+    	<div class="col-xs-3" style="text-align: center;">
+
+    	<div class="list-group">
+    		 <a href="view_order_details.php" class="list-group-item ">View Order Details</a>
+    		<a href="view_food_items.php" class="list-group-item active">View Food Item</a>
+    		<a href="add_food_items.php" class="list-group-item ">Add Food Item</a>
+    		<a href="edit_food_items.php" class="list-group-item ">Edit Food Item</a>
+    		<a href="delete_food_items.php" class="list-group-item ">Delete Food Item</a>
+       
+    	</div>
+    </div>
+    
+
+
+    
+    <div class="col-xs-9">
+      <div class="form-area" style="padding: 0px 100px 100px 100px;">
+        <form action="" method="POST">
+        <br style="clear: both">
+          <h3 style="margin-bottom: 25px; text-align: center; font-size: 30px;"> YOUR FOOD ITEMS LIST </h3>
+
+
+<?php
+
+
+
+
+// Storing Session
+$user_check=$_SESSION['login_user1'];
+$sql = "SELECT * FROM food ORDER BY f_id";
+$result = mysqli_query($conn, $sql);
+
+
+if (mysqli_num_rows($result) > 0)
+{
+
+  ?>
+
+  <table class="table table-striped">
+    <thead class="thead-dark">
+      <tr>
+        <th>  </th>
+        <th> FOOD ID </th>
+        <th> FOOD Name </th>
+        <th> FOOD Price </th>
+	<th> FOOD Type </th>
+        <th> Description </th>
+        
+      </tr>
+    </thead>
+
+    <?PHP
+      //OUTPUT DATA OF EACH ROW
+      while($row = mysqli_fetch_assoc($result)){
+    ?>
+
+  <tbody>
+    <tr>
+      <td> <span class="glyphicon glyphicon-menu-right"></span> </td>
+      <td><?php echo $row["f_id"]; ?></td>
+      <td><?php echo $row["f_name"]; ?></td>
+      <td><?php echo $row["f_rate"]; ?></td>
+	<td><?php echo $row["f_type"]; ?></td>
+      <td><?php echo $row["f_description"]; ?></td>
+      
+    </tr>
+  </tbody>
+  
+  <?php } ?>
+  </table>
+    <br>
+
+
+  <?php } else { ?>
+
+  <h4><center>0 RESULTS</center> </h4>
+
+  <?php } ?>
+
+        </form>
+
+        
+        </div>
+      
+    </div>
+</div>
+<br>
+<br>
+<br>
+<br>
+  
+  </body>
+</html>
